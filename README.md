682a9c17e4529db85d724fb6a24b450d9343c879
Kovács Levente <kovilevi98@gmail.com>
2022-04-12 17:32:08 +0200

    init project

--- README.md
+++ README.md
@@ -1,39 +1,48 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# platform_text_input
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
+## Introduction
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
+This project provides an amazing widget for using the specific inputfield for the specific platform
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+### Example
 
-## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+```dart
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+          child: MTextFormField(
+          controller: _usernameController,
+          focusNode: _usernameFocus,
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.next,
+          inputDecorator: InputDecoration(
+            labelText: "username",
+            errorText: "username error",
+          ),
+          onChanged: (value) {
+            _store.username = value;
+          },
+          onFieldSubmitted: (_) {
+            _emailFocus.requestFocus();
+      },
+    );
+  }
+}
+```
+    
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
-
-```dart
-const like = 'sample';
+Run this command for installation
 ```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
+flutter pub add platform_text_input
+```
+Or add this dependency
+```
+dependencies:
+  platform_text_input: ^0.0.1
+```
--- lib/platform_text_input.dart
+++ lib/platform_text_input.dart
@@ -1,7 +1,136 @@
 library platform_text_input;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PlatformTextInput extends StatelessWidget {
+  final TextInputAction textInputAction;
+  final TextInputType keyboardType;
+  final TextEditingController? controller;
+  final InputDecoration inputDecorator;
+  final FocusNode? focusNode;
+  final bool readOnly;
+  final bool enabled;
+  final void Function()? onTap;
+  final void Function(String) onChanged;
+  final void Function(String)? onFieldSubmitted;
+  final List<TextInputFormatter>? inputFormatters;
+  final bool obscureText;
+  final int? minLines;
+  final double? verticalPadding;
+  final double? horizontalPadding;
+  final double? borderRadius;
+  final TextStyle? hintTextStyle;
+  final TextStyle? textStyle;
+
+  PlatformTextInput({
+    Key? key,
+    required this.textInputAction,
+    this.keyboardType = TextInputType.text,
+    required this.controller,
+    required this.onChanged,
+    this.onTap,
+    this.inputFormatters,
+    this.onFieldSubmitted,
+    required this.inputDecorator,
+    this.focusNode,
+    this.readOnly = false,
+    this.enabled = true,
+    this.obscureText = false,
+    this.minLines,
+    this.verticalPadding = 16,
+    this.horizontalPadding = 8,
+    this.borderRadius = 10,
+    this.hintTextStyle,
+    this.textStyle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+            color: inputDecorator.fillColor ??
+                Theme.of(context).inputDecorationTheme.fillColor,
+            borderRadius: BorderRadius.circular(borderRadius!),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: CupertinoTextFormFieldRow(
+                  minLines: minLines,
+                  maxLines: (minLines != null) ? minLines! + 10 : 1,
+                  textAlignVertical: TextAlignVertical.center,
+                  textInputAction: textInputAction,
+                  keyboardType: keyboardType,
+                  placeholderStyle: (hintTextStyle != null) ? hintTextStyle : TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  style: (textStyle != null) ? textStyle : TextStyle(
+                    color:
+                        enabled ? Colors.black : Colors.black.withOpacity(0.6),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    vertical: verticalPadding!,
+                    horizontal: horizontalPadding!,
+                  ),
+                  readOnly: readOnly,
+                  cursorHeight: 28,
+                  onChanged: onChanged,
+                  controller: controller,
+                  focusNode: focusNode,
+                  autofocus: false,
+                  enabled: enabled,
+                  obscureText: obscureText,
+                  placeholder: inputDecorator.labelText,
+                  validator: (s) => inputDecorator.errorText,
+                  autovalidateMode: AutovalidateMode.always,
+                  onFieldSubmitted: onFieldSubmitted,
+                  inputFormatters: inputFormatters,
+                ),
+              ),
+              if (inputDecorator.suffixIcon != null)
+                Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: inputDecorator.suffixIcon!,
+                ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return TextFormField(
+        minLines: minLines,
+        maxLines: (minLines != null) ? minLines! + 10 : 1,
+        textAlignVertical: TextAlignVertical.center,
+        textInputAction: textInputAction,
+        keyboardType: keyboardType,
+        onChanged: onChanged,
+        controller: controller,
+        focusNode: focusNode,
+        autofocus: false,
+        enabled: enabled,
+        readOnly: readOnly,
+        obscureText: obscureText,
+        decoration: inputDecorator,
+        style: (textStyle != null) ? textStyle : Theme.of(context).textTheme.subtitle1!.copyWith(
+              color: enabled
+                  ? Colors.black
+                  : Colors.black.withOpacity(0.6),
+            ),
+        onFieldSubmitted: onFieldSubmitted,
+        inputFormatters: inputFormatters,
+        onTap: onTap,
+      );
+    }
+  }
 }
